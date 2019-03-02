b83e7456f0591ee8a7c2572e5bb2ef74a4c46000
Awote <37435132+Awote@users.noreply.github.com>
2019-03-02 15:12:40 +0700

    невозрастающая последовательность

--- 11_ss.c
+++ 11_ss.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 int main(void)
 {
-int n = 0,x = 0,y = 0;
-scanf("%d", &n);
-scanf("%d",&y);
-for (int i=1;i<n;i++)
-{
-if ((i%2)==1)  
-{
-scanf("%d",&x); if (y>=x) printf("1\n"); else {printf("0\n");
-}}
-if ((i%2)==0){scanf("%d",&y); if(x>=y) printf("1\n"); else printf (" 0\n");}
+    int n = 0,x = 0,y = 0,rez =-1,enter_num = 0;
+    scanf("%d", &n);
+    scanf("%d",&y);
+    for (int i=1;i<n;i++)
+        {
+scanf("%d",&enter_num);
+        if ((i%2)==1 && rez != 0)  
+             {
+             x = enter_num; if (y>=x) rez = 1; else rez = 0;
+             }
+        if ((i%2)==0 && rez != 0){y=enter_num; if(x>=y) rez = 1; else rez=0 ; 
+             }
 
 }
+printf("%d",rez);
 }
